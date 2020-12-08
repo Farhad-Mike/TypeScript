3a9bb920685e3f5d801a5428b8cb1094e66a7c09
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2020-12-09 03:36:49 +0400

    u

--- note.ts
+++ note.ts
@@ -2,6 +2,8 @@ let bool: boolean = true;
 let num: number = 0xf00d;
 let str: string = 'name';
 let mlstr: string = `string`;
+let not: void = null;
+let not_2: void = undefined;
 let mixed: number|boolean; // Позволит задавать два типа значений
 let arrNum: number[] = [1, 2, 3];
 let arrNum_2: Array<number> = [1, 2, 3];
@@ -10,6 +12,10 @@ let arrStr_2: Array<string> = ['s', 't', 'r'];
 let arrMix: (string|number)[] = []; // Теперь можно использовать 2 типа значений внутри массива.
 let arrAny: any[] = [1, 'str', true]; // Создает массив самый обычный массив без типизации
 let tupl: [string, number, boolean] = ['str', 12, true]; // Tuple имеет фиксированную длину и может иметь иметь разные типы элементов в зависимости от позиции.
+let func: Function = () => {console.log('hello, world')};
+let func_1: Function = (a: number, b: string, c: number | string = 10, d: boolean = false, i?: boolean) {}; // Параметры а и b обязательны и должны быть числом и строкой. Параметр с необезательный и по умолчанию задается 10, а так же должен быть строкой или числом. Параметр d должен быть булевым и необязательный, по умолчанию задается false. Параметр i необязательный.
+let func_2 = (a: number, b: number): number => {}; // Это говорит о том что функция вернет тип number
+let func_3: (a: number, b:number) => void; // Это подготовительный макет. То есть при создании функции с названием func_3 нужно писать так func_3 = (uid: number, age: number) {console.log(uid * number)};
 let obj: object = {
     name: 'Farhad',
     wife: 'Ayten',
@@ -21,6 +27,18 @@ let obj_1: {    // Задать какие типы и сколько свойс
     age: number,
     married: boolean,
 }
+
+type StrOrNum = string | number;
+type objWithName = {name: string, uid: StrOrNum};
+function getUserLog(user: objWithName) {}; // Принимает объект user со свойствами name и uid;
+
+let logDetail: (obj: {name: string, age: number}) => void; // По этому макету будет создана функция которая выглядит след образом. (user: {name: string, age: number}): void => {};
+
+
+
+
+
+
 
 enum Num {zero, one, two};  // Тип enum - это более удобный способ задания понятных имен набору численных значений.
 let num: Num = Num.zero;    // Result: 0;
